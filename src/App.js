6e06bbaa5cf8edce0ff8de659b15886bb3a7354a
LucasZornoff <lucas.clal@gmail.com>
2023-06-01 14:34:05 -0300

    avançando no projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Logo from './components/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [{ img: perfil, alt: 'perfil' }, { img: sacola, alt: 'sacola' }]
+
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo/>
+        <ul className='opcoes'>
+          { textoOpcoes.map( (texto) => ( 
+            <li className='opcao'><p>{texto}</p></li>
+          ) ) }
+        </ul>
+
+        <ul className='icones'>
+            { icones.map( (icone) => (
+              <li className='icone'><img src={icone.img} alt={icone.alt}></img></li>
+            ))}
+        </ul>
       </header>
     </div>
   );
